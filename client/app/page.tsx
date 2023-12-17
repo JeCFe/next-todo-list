61a6d4228de48ef4deed08f4c406d15dab6bd7e8
Jessica Fealy <jessicaf091@outlook.com>
2023-12-17 01:13:39 +0000

    Add boxes for filter and sort

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,18 +3,73 @@ import { useGetTodos } from "@/hooks/useGetTodos";
 
 import { getApiClient } from "../services";
 import { ArrowDown, ArrowUp, Delete } from "@/components";
+import { useEffect, useMemo } from "react";
 
 const issue = getApiClient().path("/todo/add").method("post").create();
 
 export default function Home() {
   const { data, isLoading: todoLoading, error: todoError } = useGetTodos();
 
+  const filteredTodos = useMemo(() => {
+    return data;
+  }, [data]); // sets up the memo for filters
+
+  const filterTags = useMemo(() => {
+    if (!filteredTodos) {
+      return undefined;
+    }
+    const tags = filteredTodos
+      .map((todo) => todo.tags)
+      .filter((tag): tag is string => tag !== null && tag !== undefined);
+
+    if (tags.length === 0) {
+      return undefined;
+    }
+
+    return [...new Set(tags)];
+  }, [filteredTodos]);
+
+  const filteredColours = useMemo(() => {
+    if (!filteredTodos) {
+      return undefined;
+    }
+    const tags = filteredTodos
+      .map((todo) => todo.colour)
+      .filter(
+        (colour): colour is string => colour !== null && colour !== undefined
+      );
+
+    if (tags.length === 0) {
+      return undefined;
+    }
+
+    return [...new Set(tags)];
+  }, [filteredTodos]);
+
   if (todoLoading) return <div>Loading...</div>;
   else if (data === undefined) return <div>No todos. Why not create one?</div>;
   return (
     <div className="container mx-auto">
-      <div className="flex flex-col md:flex-row">
-        <div className="flex flex-col"></div>
+      <div className="flex flex-col md:flex-row md:space-x-2 space-y-2 md:space-y-0 ">
+        <div className="flex flex-row md:flex-col space-x-2 md:space-x-0 md:space-y-2">
+          <div className="flex flex-col border border-black rounded-xl p-2 space-y-2 w-1/2 md:w-fit">
+            <h2 className="text-lg">Filter</h2>
+            <h3 className="underline underline-offset-4">Tags</h3>
+            {filterTags !== undefined
+              ? filterTags.map((tag) => <div key={tag}>{tag}</div>)
+              : "No tags available"}
+            <h3 className="underline underline-offset-4">Colours</h3>
+            {filteredColours !== undefined
+              ? filteredColours.map((colour) => (
+                  <div key={colour}>{colour}</div>
+                ))
+              : "No colours available"}
+          </div>
+          <div className="flex flex-col border border-black rounded-xl p-2 space-y-2 w-1/2 md:w-fit">
+            <h2 className="text-lg">Sort</h2>
+            ADD SORTING STUFF
+          </div>
+        </div>
 
         <div className="flex w-full flex-col space-y-4 border border-black rounded-xl p-4">
           {data.map((todo, index) => (
