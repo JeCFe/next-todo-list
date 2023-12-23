a8a39df7d60101f5e21e6ae10bdd49293946c5f8
Jessica Fealy <jessicaf091@outlook.com>
2023-12-23 22:34:18 +0000

    More responsive user details component

--- client/components/header/User.tsx
+++ client/components/header/User.tsx
@@ -2,7 +2,7 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 
 export function User() {
-  const { user, error, isLoading } = useUser();
+  const { user, isLoading } = useUser();
   return (
     <div>
       {isLoading ? (
@@ -10,19 +10,22 @@ export function User() {
       ) : (
         <div>
           {user ? (
-            <div className="flex flex-row divide-x">
-              <div className="flex flex-row space-x-4 pr-2">
+            <div className="flex flex-col sm:flex-row divide-x">
+              <div className="flex flex-row space-x-4 pr-0 sm:pr-2">
                 <p className="flex items-center">{user.name ?? user.email}</p>
 
                 {/* eslint-disable-next-line @next/next/no-img-element*/}
                 <img
                   src={user.picture ?? ""}
                   alt="User profile picture"
-                  className="w-14 h-14 rounded-full flex items-center"
+                  className="w-14 h-14 rounded-full items-center hidden sm:flex"
                 />
               </div>
 
-              <a className="pl-2 flex items-center" href="/api/auth/logout">
+              <a
+                className="pl-0 sm:pl-2 flex items-center"
+                href="/api/auth/logout"
+              >
                 Logout
               </a>
             </div>
