ecd6dec5de664bb984b90c10adafe692296ebcfd
Jessica Fealy <jessicaf091@outlook.com>
2023-12-23 20:11:00 +0000

    Updated to radio button

--- client/components/radio-button/RadioButton.tsx
+++ client/components/radio-button/RadioButton.tsx
@@ -3,25 +3,26 @@ import { cva, VariantProps } from "class-variance-authority";
 
 const radio = cva(
   [
-    "apperance-none glex rounded-full border-2 border-black",
-    "after:m-auto after:flex after:content-center after:justify-center after:rounded-full after:checked:bg-black",
-    "@apply focus:border-4 focus:ring focus:red",
+    "appearance-none flex rounded-full border-2 border-black",
+    "after:m-auto after:flex after:content-center after:justify-center after:rounded-full after:checked:bg-pink-400",
+    "@apply focus:border-4 focus:ring focus:ring-red-500",
   ],
   {
     variants: {
       size: {
-        medium: ["h-10 min-h-10 w-10 min-w-10", "after:h-5 after:w-5"],
+        small: ["h-8 min-h-8 w-8 min-w-8", "after:h-4 after:w-4"],
+        medium: ["h-10 min-h-10 w-10 min-w-10", "after:h-6 after:w-6"],
       },
     },
     defaultVariants: {
-      size: "medium",
+      size: "small",
     },
   }
 );
 
 const label = cva(["flex flex-col"], {
-  variants: { size: { medium: "text-lg" } },
-  defaultVariants: { size: "medium" },
+  variants: { size: { medium: "text-lg", small: "text-base" } },
+  defaultVariants: { size: "small" },
 });
 
 type Props = {
@@ -35,9 +36,8 @@ export const RadioButton = forwardRef<HTMLInputElement, Props>(
   ({ children, hint, size, className, ...rest }: Props, ref) => (
     <label className={label({ size, className })}>
       <div className="flex flex-row items-center">
-        <input {...rest} className={radio({ size })} ref={ref} type="radio">
-          <span className="ml-4">{children}</span>
-        </input>
+        <input {...rest} className={radio({ size })} ref={ref} type="radio" />
+        <span className="ml-2">{children}</span>
       </div>
       {hint && <span className="ml-14 text-gray-500">{hint}</span>}
     </label>
