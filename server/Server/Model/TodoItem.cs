d165711b8939706b67a803a85f3fdd6073cbb114
Jessica Fealy <jessicaf091@outlook.com>
2023-12-24 17:48:14 +0000

    Making colour a required type

--- server/Server/Model/TodoItem.cs
+++ server/Server/Model/TodoItem.cs
@@ -4,7 +4,7 @@ public record TodoItem
 {
     public required string Text { get; init; }
     public required string Version { get; init; }
-    public string? Colour { get; init; }
+    public required string Colour { get; init; } // Ideally this should be a type
     public string? Tags { get; init; }
     public required DateTime Created { get; init; }
     public DateTime? Closed { get; init; }
